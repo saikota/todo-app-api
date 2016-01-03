4cb04be1c10a3fbb8f620acd699646c75509cdb4
unknown <M853684@L4688898.federated.fds>
2016-01-02 21:52:24 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+ var server=require('./server');
+ var port=8080||Number(process.env.SERVER_PORT)
+ server.listen(port,function(){
+	 console.log("todo server is listening on %j",server.address());
+ });
+
+ require("./routes")
