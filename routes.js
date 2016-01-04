9df70e95560f716268a41488f2d926f5dc58e6ed
unknown <M853684@L4688898.federated.fds>
2016-01-04 06:18:13 -0800

    adding in validations.

--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 var server=require('./server');
 var resources=require('./resources');
-
+var validating=require('./middleware/validating')
+var schemas=require('./schemas');
 //lists
 server.get("/lists",resources.lists.list);
-server.put("/lists/:list",resources.lists.create);//put /lists/myList
+server.put("/lists/:list",validating(schemas.list),resources.lists.create);//put /lists/myList
 server.del("/lists/:list",resources.lists.del);
 
 //Items
 server.get('/lists/:list/items',resources.items.list);
-server.post('/lists/:list/items',resources.items.create);
+server.post('/lists/:list/items',validating(schemas.item),resources.items.create);
 server.del('lists/:list/items/:item',resources.items.del);
